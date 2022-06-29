e32e3bbd203924d424eea180ac7a0cb696afdf91
YUN SOHEE <84841632+sh5124y@users.noreply.github.com>
2022-06-29 22:27:49 +0900

    수업내용 필기, null true False

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const a = 5;
-const b = 2;
-let myName = "sohee";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log ("hello" + myName);
-
-myName = "yunsohee";
-
-console.log("your new name is " + myName);
+const amIFat = true;
+console.log (amIFat);
