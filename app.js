71ad161c42f35f01a381c3cb97382370440d5773
YUN SOHEE <84841632+sh5124y@users.noreply.github.com>
2022-07-03 22:59:05 +0900

    array

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const amIFat = null;
-let something;
-console.log (something, amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfweek = [mon,tue,wed,thu,fri,sat,sun];
+
+const nonsense = [1,2,"hello",false,null,true,nusefinde,"sohee"];
+
+console.log(daysOfweek,nonsense);
